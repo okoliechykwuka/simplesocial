d8c0de89b9af5c71229021f8b31f210374dfc649
Chukypedro <forbeseuroupe@gmail.com>
2020-09-06 02:10:06 +0100

    Update settings.py

--- simplesocial/settings.py
+++ simplesocial/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '(5qpl&b-b48^jszl@nz02#lgbw%tp(o)_wb!)b&k(j9*kueh@&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['simplesocial-app.herokuapp.com']
 
 
 # Application definition
